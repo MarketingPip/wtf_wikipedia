558a005e60c7617fc3348b33b1d56e43a63e4eb7
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2023-11-17 03:32:17 -0500

    Update getResult.js

--- src/_fetch/getResult.js
+++ src/_fetch/getResult.js
@@ -49,10 +49,7 @@ const getResult = function (data, options = {}) {
       domain: domain,
       wikidata: page.pageprops.wikibase_item,
       description: page.pageprops['wikibase-shortdesc'],
-      revisions: {
-        revID: page?.revisions[0]?.revid || null,
-        parentID: page?.revisions[0]?.parentid || null
-      },
+      revisionID: page?.revisions[0]?.revid || null,
     })
 
 
