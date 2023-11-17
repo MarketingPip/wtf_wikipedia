64f684915ded75cc2f80403a6eff51779c1bb88c
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2023-11-17 02:15:45 -0500

    Update metadata.test.js

--- tests/fetch/metadata.test.js
+++ tests/fetch/metadata.test.js
@@ -8,8 +8,10 @@ test('get metadata', (t) => {
     'Api-User-Agent': 'wtf_wikipedia test script - <spencermountain@gmail.com>',
   })
   p.then(function (doc) {
-    t.equal(doc.pageID(), 72879, 'got wikipedia id')
+    t.equal(doc.pageID(), 72879, 'got wikipedia id')//
     t.equal(doc.wikidata(), 'Q132880', 'got wikidata id')
+    t.equal(doc.revisions.revID(), 1185022205, 'got revision id')//
+    t.equal(doc.revisions.parentID(), 1183547317, 'got revision parent id')//
   })
   p.catch(function (e) {
     t.throw(e)
